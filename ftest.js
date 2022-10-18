37a09f643ab9813aabf858a264fb0b6cd1f63d0c
joshua <94032950+joshuaSYSS@users.noreply.github.com>
2022-10-18 13:05:08 +0800

    Update ftest.js

--- ftest.js
+++ ftest.js
@@ -162,6 +162,16 @@ function compile(){
     }
     alert(e);
 }
+async function startCapture(displayMediaOptions) {
+  let captureStream = null;
+
+  try {
+    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch (err) {
+    console.error(`Error: ${err}`);
+  }
+  return captureStream;
+}
 //unused
 function sleep(milliseconds) {
   const date = Date.now();
